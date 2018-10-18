d20d67b299ec935adcbf3ef42512d117a8b764dd
Bennett Huffman <bennett.w.huffman@gmail.com>
2018-10-18 18:21:35 -0400

    Bennett is up and running

---
+++ ted/src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+var config = {
+  apiKey: "AIzaSyCTunhepxOuJE0aJPNWRhtbxCmCPIj29rI",
+  authDomain: "tedx-148a6.firebaseapp.com",
+  databaseURL: "https://tedx-148a6.firebaseio.com",
+  projectId: "tedx-148a6",
+  storageBucket: "tedx-148a6.appspot.com",
+  messagingSenderId: "199524458684"
+};
+const fire = firebase.initializeApp(config);
+export default fire;
