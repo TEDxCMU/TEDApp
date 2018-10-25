a44e312ed5f5fdd9f792a2ac3df489ee01d56e46
dean dijour <Dean Dijour>
2018-10-25 17:51:47 -0400

    random bs changes i'm unaware of?

--- ted/src/components/navigation.js
+++ ted/src/components/navigation.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../App.css';
+import '../../components/navigation.css';
 import {NavLink} from 'react-router-dom';
 
 export class Navigation extends Component {
