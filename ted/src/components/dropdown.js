8069c6ccc6b9d9c335d0700305b8c168d7dac0b5
Bennett Huffman <bennett.w.huffman@gmail.com>
2018-11-13 23:17:09 -0500

    create initial dropdown component

---
+++ ted/src/components/dropdown.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+import './dropdown.css';
+
+export class Dropdown extends Component {
+    render() {
+        return (
+          <div className="dropdown">
+            I'm a dropdown!
+          </div>
+        );
+      }
+}
+
+export default Dropdown;
