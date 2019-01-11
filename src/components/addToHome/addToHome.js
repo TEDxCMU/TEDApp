4110189a9d98c5a49edb256841954b6994aae2cf
Bennett Huffman <bennett.w.huffman@gmail.com>
2019-01-11 12:10:11 -0600

    improved bottom popup

--- src/components/addToHome/addToHome.js
+++ src/components/addToHome/addToHome.js
@@ -15,20 +15,18 @@ export class PopUp extends Component {
         return (
             <div style={{width: "100%"}}>
                 { this.props.iOS ? 
-                    <div id="popup">
-                        <img src={require('./plusIcon.png')} className="plus" alt="MyImage" title="MyImage" />
-                        <div>
-                            <span>Install this web app on your iPhone: Tap </span>
-                            <img src={require('./downloadIcon.png')} className="tiny" alt="MyImage" title="MyImage" />
-                            <span> and then "Add to Homescreen"</span>
+                    <div id="popup-bg">
+                        <div id="popup-content">
+                            {/* <img src={require('./plusIcon.png')} className="plus" alt="add" title="add" /> */}
+                            <h5>HEY!</h5>
+                            <p>Install this web app on your iPhone! Tap <img src={require('./downloadIcon.png')} className="download-icon" alt="download icon" title="download icon" /> and then "Add to Homescreen"</p>
+                            <button type="button" onClick={(e) => this.closeModal()}>No thanks</button>
                         </div>
-                        <button type="button" onClick={(e) => this.closeModal()}>No, thanks</button>
-
                     </div>
                 :
                 <div id="popup">
                         <h5>Tap below to add an icon to your Android home screen for quick access!</h5>
-                        <button type="button" onClick={(e) => this.closeModal()}>No, thanks</button>
+                        <button type="button" onClick={(e) => this.closeModal()}>No thanks</button>
                 </div>
                 }
             </div>
