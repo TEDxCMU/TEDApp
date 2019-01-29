e3d12426f1b0781ed8d88b82c9cb6d0fa2a4c8e6
Karena Kong <karenakong@Karenas-MBP.wv.cc.cmu.edu>
2019-01-29 03:36:33 -0500

    brainfood
    added map, location icon, and message card

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'leaflet/dist/leaflet.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
