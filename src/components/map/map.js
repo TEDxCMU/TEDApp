7445b7362b4d4715418dc101247519e44d021740
Bennett Huffman <bennett.w.huffman@gmail.com>
2019-02-05 16:58:05 -0500

    map image fix

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -22,9 +22,7 @@ export class Map extends Component {
             <h5 class="sectionTitle">Getting to CMU</h5>
             <iframe class="map" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJwTuY5SHyNIgROLIDNCm3XbE&key=AIzaSyDcMQLOO-WbqT-IopP9CmBzkmCBzoG67fQ" allowfullscreen></iframe>
             <h5 class="sectionTitle">Getting around CMU</h5>
-            <image src={map} alt="map"></image>
-
-
+            <img src={map} alt="map"></img>
         </div>
 
         );
