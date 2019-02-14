4c5c963537e6f340b30b92500e64a3ea3099831a
Dean Dijour <dzd@Deans-MBP.wv.cc.cmu.edu>
2019-02-14 13:48:11 -0500

    fixed FAQ

--- src/components/faq/faq.js
+++ src/components/faq/faq.js
@@ -9,49 +9,49 @@ export class Faq extends Component {
     state = {
         questions: [
             {
-                question: 'I’d like to know about upcoming TEDxCMU events?',
-                answer: '<p>Subscribe <a href="http://tedxcmu.us3.list-manage.com/subscribe/post?u=eb8a63a687a4d582ce3b6b515&id=ce11cda9eb">here</a>. to get on our mailing list to receive information about the conference theme, Mini-Events, Main Event details, ticket sales, and other news about TEDxCMU!<p>',
+                question: 'Where can I learn more about upcoming TEDxCMU events?',
+                answer: '<p>Subscribe <a href="http://tedxcmu.us3.list-manage.com/subscribe/post?u=eb8a63a687a4d582ce3b6b515&id=ce11cda9eb">here</a> to get on our mailing list to receive information about the conference theme, Mini-Events, Main Event details, ticket sales, and other news about TEDxCMU!<p>',
                 category: 'general'
             },
             {
-                question: 'When and where is this year’s TEDxCMU event?',
-                answer: '<p>The full-day event of curated speakers, entertainment, food and audience experiences will held Saturday, March 30th at the Jared L. Cohon University Center at Carnegie Mellon University. The Jared L. Cohon University center is located in the middle of campus and can be seen from Forbes Avenue. More information about its location and directions can be found <a href="http://www.cmu.edu/cohon-university-center/">here</a>.</p>',
+                question: 'When and where is this year\'s Spring Mini-Event?',
+                answer: '<p>This year\'s Spring Mini-Event will be held on February 14th, 2019 at Hamerschlag B 103 at 6:30 PM - 7:30 PM.</p>',
                 category: 'general'
             },
             {
                 question: 'Will there be food? What are the lunch options?',
-                answer: '<p>We will provide some light breakfast in the morning during registration. There will be a lunch break with food and beverages (including vegetarian and gluten free options).</p>',
+                answer: '<p>Yes! We will provide free donuts and coffee.</p>',
                 category: 'general'
             },
             {
-                question: 'How much are tickets?',
-                answer: '<p>The 2019 prices will come out soon and are set for general admission and VIP admission. TEDxCMU relies on tickets and support to cover all our costs per TED rules for independently organized TEDx events.</p>',
+                question: 'How much are Spring Mini-Event tickets?',
+                answer: '<p>The Spring Mini-Event is free!</p>',
                 category: 'tickets'
             },
             {
-                question: 'Can I return my tickets? Can I exchange my tickets?',
-                answer: '<p>Tickets are non-refundable. Tickets can be transferred, however, we cannot make the transfer for you.</p>',
+                question: 'Can I return main event tickets? Can I exchange my tickets?',
+                answer: '<p>For our main event, tickets are non-refundable. Tickets can be transferred, but we cannot make the transfer for you.</p>',
                 category: 'tickets'
             
             },
             {
-                question: 'When do tickets go on sale?',
+                question: 'When do main event tickets go on sale?',
                 answer: '<p>Our tickets traditionally go on sale in early March. We may have events that raffle off special discounts or give free tickets. To learn more about those events, <a href="http://tedxcmu.us3.list-manage.com/subscribe/post?u=eb8a63a687a4d582ce3b6b515&id=ce11cda9eb">subscribe</a> to our newsletter and follow us on <a href="http://facebook.com/tedxcmu">social media</a>.</p>',
                 category: 'tickets'
             },
             {
                 question: 'Are kids allowed? Are there age minimums? Will teenagers like TEDxCMU?',
-                answer: '<p>We work to keep the day engaging for all attendees, with experiences designed  “for your seat” and “for your feet” in the auditorium and the lobby. Older teenagers will likely get the most out of the event, but you know your kids best. Will they enjoy it?</p>',
+                answer: '<p>We work to keep the day engaging for all attendees, with experiences designed  "for your seat" and "for your feet" in the auditorium and the lobby. Older teenagers will likely get the most out of the event, but you know your kids best. Will they enjoy it?</p>',
                 category: 'tickets'
             },
             {
                 question: 'Is there assigned seating or is it first come, first served? We bought different tiers of tickets, can we sit together? What about accessibility?',
-                answer: '<p>VIP ticket holders and special guests are allowed early access to seating on the day of the event. Rows are reserved for each ticket tier and is on a first come, first served basis for seating within the respective ticket tier.</p>',
+                answer: '<p>Seating is based on first come, first serve for mini events.</p><p>For the main event, VIP ticket holders and special guests are allowed early access to seating on the day of the event. Rows are reserved for each ticket tier and is on a first come, first served basis for seating within the respective ticket tier.</p>',
                 category: 'tickets'
             },
             {
-                question: 'I’d like to volunteer.',
-                answer: '<p>Thank you for your interest in joining the team! TEDxCMU wouldn’t exist without the contributions volunteers make at our events. We select our day-of-event volunteers a month to two in advance of the event. You can also scroll to the bottom of the page to subscribe to our newsletter and receive all the announcements about future ticket sales, adventures, salons and more. We look forward to seeing you!</p>',
+                question: 'I\'d like to volunteer.',
+                answer: '<p>Thank you for your interest in joining the team! TEDxCMU wouldn\'t exist without the contributions volunteers make at our events. We select our day-of-event volunteers a month to two in advance of the event. You can also <a href="http://tedxcmu.us3.list-manage.com/subscribe/post?u=eb8a63a687a4d582ce3b6b515&id=ce11cda9eb">subscribe</a> to our newsletter and receive all the announcements about future ticket sales, adventures, salons and more. We look forward to seeing you!</p>',
                 category: 'volunteer'
             },
             {
