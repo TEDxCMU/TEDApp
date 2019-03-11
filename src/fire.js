39bf511ca4acae1d9aceab9bc340d202be3d7416
Dean Dijour <dzd@Deans-MacBook-Pro.local>
2019-03-11 00:45:47 -0400

    trying one last thing, otherwise might give up on env variables

--- src/fire.js
+++ src/fire.js
@@ -7,7 +7,7 @@ var config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  projectId: "tedx-148a6",
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
