138893dbb1612ffbd41dd0204ddccaa88fea14f6
Dean Dijour <dijour@cmu.edu>
2019-03-25 15:05:43 -0400

    added alt tag to map img

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -14,7 +14,7 @@ export class Map extends Component {
             <iframe name="map" className="map" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJwTuY5SHyNIgROLIDNCm3XbE&key=AIzaSyCTunhepxOuJE0aJPNWRhtbxCmCPIj29rI" title="map" allowFullScreen
             ></iframe>
             <h5 className="sectionTitle">Getting to the Event</h5>
-            <img name="mcconomy" className="indoor-map" src={map}></img>
+            <img name="mcconomy" className="indoor-map" alt="indoor-map" src={map}></img>
         </div>
         );
       }
